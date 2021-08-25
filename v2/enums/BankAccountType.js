61a437afc00c7f1a932ef473df09e8b42d9bbea5
vitortramos <44622689+vitortramos@users.noreply.github.com>
2021-08-25 18:23:00 -0300

    Update BankAccountType.js

--- v2/enums/BankAccountType.js
+++ v2/enums/BankAccountType.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var BankAccountType;
 (function (BankAccountType) {
-  BankAccountType["CHECKINGS"] = "CHECKINGS";
+  BankAccountType["CHECKING"] = "CHECKING";
   BankAccountType["SAVINGS"] = "SAVINGS";
 })((BankAccountType = exports.BankAccountType || (exports.BankAccountType = {})));
 //# sourceMappingURL=BankAccountType.js.map
